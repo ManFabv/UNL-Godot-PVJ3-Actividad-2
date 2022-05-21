177e862b2e10e35ca98366061ba5021384740a2c
Fabricio Manrique <fabemanrique@hotmail.com>
2022-05-21 19:03:12 -0300

    Made health a setter

--- player/player_base.gd
+++ player/player_base.gd
@@ -4,7 +4,7 @@ class_name PlayerBase
 export (float) var SPEED = 160
 export (float) var MAX_HEALTH = 100
 
-onready var health = MAX_HEALTH
+onready var health = MAX_HEALTH setget set_health
 
 var dead = false
 
@@ -16,15 +16,17 @@ func _ready():
 
 
 func hurt(amount):
-	health-=amount
-	#esto podría ir en un setter
+	self.health-=amount
+
+
+func set_health(new_health) -> void:
+	health = new_health
 	if health <= 0:
+		health = 0
 		if !dead:
 			emit_signal("im_dead")
 			dead = true
-			set_physics_process(false) 
-		health = 0
-		return
-	elif health > 100:
-		health = 100
-	emit_signal("health_change",health)
+			set_physics_process(false)
+	elif health > MAX_HEALTH:
+		health = MAX_HEALTH
+		emit_signal("health_change",health)
